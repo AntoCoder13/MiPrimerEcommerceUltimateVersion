fada0ce347f48262f52a236f4841e1b26e6c7b68
AntoCoder13 <antonellaargisanchez@gmail.com>
2023-10-24 22:40:15 -0300

    falta CSS

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,18 @@
 import React from "react"
 import { Link } from 'react-router-dom'
+import CartWidget from '../CartWidget/CartWidget'
 
 const NavBar = () => {
     return (
         <nav className="navbar">
-                <Link href="/" className="logo"> <h1>ANTOSHOP</h1> </Link>
-                <ul className="menu">
-                    <li> </li>
-                </ul>
+            <Link href="/" className="logo"> <h1>ANTOSHOP</h1> </Link>
+            <ul className="menu">
+                <li><Link className="menu-link" to="/">Inicio</Link></li>
+                <li><Link className="menu-link" to="/Nosotros">Nosotros</Link></li>
+                <li><Link className="menu-link" to="/Contactactanos">Contacto</Link></li>
+                <li><Link className="menu-link" to="/Encuentranos">Encuentranos</Link></li>
+                <li><CartWidget /></li>
+            </ul>
         </nav>
     )
 }
