53cb82a47901a973b0bb95671a8304d08abc9bdb
AntoCoder13 <antonellaargisanchez@gmail.com>
2023-10-25 20:37:38 -0300

    falta ver-mas

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -7,9 +7,9 @@ const Item = ( {producto} ) => {
     <div className="producto">
         <img src={producto.brand} alt={producto.titulo} />
         <div>
-            <h4>{producto.titulo}</h4>
-            <p>Precio: ${producto.precio}</p>
-            <p>Categoría: {toCapital(producto.categoria)}</p>
+            <h4>{producto.product_name}</h4>
+            <p>Precio: ${producto.price}</p>
+            <p>Categoría: {toCapital(producto.category)}</p>
             <Link className="ver-mas" to={`/item/${producto.id}`}>Ver más</Link>
         </div>
     </div>
